7652e56341520ee0ed35dfdf9aa5d68b6f7a6c97
MrKekson <beer.olius@gmail.com>
2016-11-01 22:52:15 +0100

    package update

--- src/homer_web/main.ts
+++ src/homer_web/main.ts
@@ -1,6 +1,19 @@
-import * as $ from "jquery";
-
-
+//import {bootstrap} from "@angular/platform-browser"
+import {Component} from "@angular/core";
 
 console.log("Start Client");
+
+@Component({
+    selector : "app",
+    template: "<div><span>Yolo</span></div>"
+
+})
+
+class appRoot{
+
+    name : "Homer";
+    status: "--"
+
+}
+
 
